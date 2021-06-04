d7cdd07eb5c56bb5102a2d103ae076ccd6911796
vijval1410 <63463705+vijval1410@users.noreply.github.com>
2021-06-04 17:22:18 +0530

    Add files via upload

---
+++ StandardDeviation.py
@@ -0,0 +1,24 @@
+import csv
+import math
+with open('data.csv',newline='')as f:
+    reader=csv.reader(f)
+    file_data=list(reader)
+data=file_data[0]
+def mean(data):
+    n=len(data)
+    total=0
+    for x in data:
+        total+=int(x)
+    mean=total/n
+    return mean
+squared_list=[]
+for i in data:
+    a=int(i)-mean(data)
+    a=a**2
+    squared_list.append(a)
+sum=0
+for i in squared_list:
+    sum=sum+i 
+result=sum/(len(data)-1)
+std_deviation=math.sqrt(result)
+print("The standard deviation is : "+str(std_deviation))
